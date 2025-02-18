c5f42cc057d9268faedc338ff7c417b5e1df797a
eaksham <akshameyal@gmail.com>
2025-02-18 12:53:24 +0200

    fix token, add uniq_email

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,18 +8,21 @@ which can be used across all test cases.
 import pytest
 from utils.api_client import APIClient
 
+
 # Fixture to provide the base URL for the API tests
 @pytest.fixture(scope="session")
 def base_url():
     return "https://gorest.co.in/public/v2"
 
+
 # Fixture to provide headers for all API requests (including authorization token)
 @pytest.fixture(scope="session")
 def headers():
     return {
-        "Authorization": "7e6d373951333152f3a69af545286eef355ebf017390a3a0192f3b06e6376672",  # Add your API token here
+        "Authorization": "Bearer 7e6d373951333152f3a69af545286eef355ebf017390a3a0192f3b06e6376672",  # Add your API token here
         "Content-Type": "application/json"
     }
+
 
 # Fixture to create an instance of the API client with the base URL and headers
 @pytest.fixture(scope="session")
--- tests/test_user.py
+++ tests/test_user.py
@@ -5,22 +5,26 @@ This file contains API tests for user operations such as Create, Read, Update, a
 
 import pytest
 import allure
+import uuid
 
 @allure.feature("User API")
 @allure.story("Create a new user")
 def test_create_user(api_client):
     """
     Test the creation of a new user using the API.
+    create uniq_email, so we will be able to run the same test several times
     """
+    uniq_email = f"user.test{uuid.uuid4().hex}@test.com"
     data = {
-        "name": "Test User",
-        "email": "test.user@example.com",
+        "name": "Test ser",
+        "email": uniq_email,
         "gender": "male",
         "status": "active"
     }
     response = api_client.post("/users", data)
     assert response.status_code == 201
     assert response.json()["name"] == data["name"]
+
 
 @allure.feature("User API")
 @allure.story("Get all users")
